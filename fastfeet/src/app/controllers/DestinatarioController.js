16d5d3bbb4b34d37100476ace7bdb75682571c7c
Elias Faiçal <eliasfaical@gmail.com>
2020-02-06 00:07:02 -0300

    Validação de dados do destinatário

--- fastfeet/src/app/controllers/DestinatarioController.js
+++ fastfeet/src/app/controllers/DestinatarioController.js
@@ -1,7 +1,24 @@
+import * as Yup from 'yup';
 import Destinatarios from '../models/Destinatarios';
 
 class DestinatarioController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.string(),
+      complemento: Yup.string().required(),
+      estado: Yup.string().required(),
+      cidade: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(400)
+        .json({ error: 'Validação do destinatário falhou' });
+    }
+
     const {
       id,
       name,
