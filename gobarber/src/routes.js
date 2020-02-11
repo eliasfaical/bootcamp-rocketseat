32ae9246e1468e80ef786b2bce855cd2a99897aa
Elias Faiçal <eliasfaical@gmail.com>
2020-02-10 23:57:48 -0300

    Marcar notificação como lidas

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -24,13 +24,14 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
