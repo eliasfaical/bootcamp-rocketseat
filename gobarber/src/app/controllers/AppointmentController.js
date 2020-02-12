8db40e8c1c399957fe71216430d94957d0658fdf
Elias Faiçal <eliasfaical@gmail.com>
2020-02-12 00:23:05 -0300

    Configurando nodemailer

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schema/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   /**
@@ -115,9 +116,17 @@ class AppointmentController {
    * Cancelar agendamento
    */
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
-    if (appointment.user_id !== req.userID) {
+    if (appointment.user_id !== req.userId) {
       return res
         .status(401)
         .json({ error: 'Você não tem permissão para fazer cancelamento' });
@@ -134,6 +143,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
