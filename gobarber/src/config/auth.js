e8802d67b602b6732bc6570f5533663efaffd86a
Elias Faiçal <eliasfaical@gmail.com>
2020-02-17 23:52:01 -0300

    Tratamentos de exceções e Variáveis de ambiente

--- gobarber/src/config/auth.js
+++ gobarber/src/config/auth.js
@@ -1,4 +1,5 @@
 export default {
-  secret: 'c53d5aeda5f76d5883cc10e486777c95',
+  // secret: 'c53d5aeda5f76d5883cc10e486777c95',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- gobarber/src/config/mail.js
+++ gobarber/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'e0f7c4fe2e1d2a',
-    pass: 'f47a83925e0b1f',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreplay@gobarber.com.br>',
