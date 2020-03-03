aa73f853041715eb193dab66e3d43e6598a2b7d6
Elias Faiçal <eliasfaical@gmail.com>
2020-03-03 14:33:46 -0300

    Corrigindo validação no CRUD de encomendas

--- fastfeet/src/app/controllers/OrdersController.js
+++ fastfeet/src/app/controllers/OrdersController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
+import { parseISO, isAfter } from 'date-fns';
 
 import Order from '../models/Order';
+
 import DeliveryMan from '../models/DeliveryMan';
 import Destinatarios from '../models/Destinatarios';
 import File from '../models/File';
@@ -8,7 +10,7 @@ import File from '../models/File';
 class OrdersController {
   // Listar
   async index(req, res) {
-    const orders = await Order.findAll({
+    const packageOrders = await Order.findAll({
       attributes: ['id', 'product', 'start_date', 'canceled_at', 'end_date'],
       include: [
         {
@@ -19,7 +21,15 @@ class OrdersController {
         {
           model: Destinatarios,
           as: 'recipient',
-          attributes: ['id', 'name'],
+          attributes: [
+            'id',
+            'name',
+            'rua',
+            'numero',
+            'complemento',
+            'estado',
+            'cep',
+          ],
         },
         {
           model: File,
@@ -29,7 +39,7 @@ class OrdersController {
       ],
     });
 
-    return res.json(orders);
+    return res.json(packageOrders);
   }
 
   // Cadastrar
@@ -38,30 +48,43 @@ class OrdersController {
       recipient_id: Yup.number(),
       deliveryman_id: Yup.number(),
       product: Yup.string(),
-      start_date: Yup.date(),
-      end_date: Yup.date(),
-      canceled_at: Yup.date(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation Fails' });
     }
 
-    const {
-      id,
-      product,
-      recipient_id,
-      deliveryman_id,
-      signature_id,
-    } = await Order.create(req.body);
+    const { product, recipient_id, deliveryman_id } = req.body;
 
-    return res.json({
-      id,
+    // Validação de destinatário
+    const recipient = await Destinatarios.findByPk(recipient_id, {
+      attributes: ['id', 'rua', 'cep'],
+    });
+
+    if (!recipient) {
+      return res.status(400).json({
+        error: 'Destinatário invalído :id.',
+      });
+    }
+
+    // Validação de entregador
+    const deliveryman = await DeliveryMan.findByPk(deliveryman_id, {
+      attributes: ['name', 'email'],
+    });
+
+    if (!deliveryman) {
+      return res.status(400).json({
+        error: 'Entregador invalído :id.',
+      });
+    }
+
+    const packageOrder = await Order.create({
       product,
       recipient_id,
       deliveryman_id,
-      signature_id,
     });
+
+    return res.json(packageOrder);
   }
 
   // Atualizar
@@ -70,42 +93,86 @@ class OrdersController {
       product: Yup.string().required(),
       recipient_id: Yup.number().required(),
       deliveryman_id: Yup.number().required(),
-      signature_id: Yup.number().required(),
+      canceled_at: Yup.date(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validação falhou' });
     }
 
-    const order = await Order.findByPk(req.params.id);
+    // checa se a encomenda existe
+    const { id } = req.params;
+    const order = await Order.findByPk(id);
 
     if (!order) {
       return res.status(400).json({ error: 'Encomenda não existe' });
     }
 
-    const {
-      id,
-      product,
-      recipient_id,
-      deliveryman_id,
-      signature_id,
-    } = await order.update(req.body);
+    // checa se destinatário existe
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const recipient = await Destinatarios.findByPk(recipient_id);
+    if (!recipient) {
+      return res.status(400).json({
+        error: 'Destinatario não encontrado. id exists?',
+      });
+    }
+
+    // checa se entregador existe
+    const deliveryman = await DeliveryMan.findByPk(deliveryman_id);
+    if (!deliveryman) {
+      return res.status(400).json({
+        error: 'Entregador não encontrado. id exists?',
+      });
+    }
+
+    const { canceled_at } = req.body;
+
+    const parseDate = parseISO(canceled_at);
+
+    const past = isAfter(new Date(), parseDate);
+
+    if (past) {
+      return res.status(401).json({
+        error: 'Você não pode canlcear essa data',
+      });
+    }
+
+    const { product } = await order.update(req.body);
 
     return res.json({
       id,
       product,
       recipient_id,
       deliveryman_id,
-      signature_id,
     });
   }
 
   // Deletar
   async delete(req, res) {
-    const order = await Order.findByPk(req.params.id);
+    const schema = Yup.object().shape({
+      id: Yup.number()
+        .integer()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.params))) {
+      return res.status(400).json({
+        error: 'Falha na validação',
+      });
+    }
+
+    const { id } = req.params;
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      return res.status(400).json({ error: 'Encomenda invalída' });
+    }
 
     await order.destroy();
-    return res.status(200).json({ ok: true });
+    return res.json({
+      message: 'Encomenda deletada com sucesso!',
+    });
   }
 }
 
