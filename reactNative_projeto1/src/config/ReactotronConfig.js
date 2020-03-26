98a0d7b9a2b08ca01af3aee2b8a294c00cac1f7a
Elias Faiçal <eliasfaical@gmail.com>
2020-03-25 23:34:27 -0300

    Finalizando primeito projeto React Native

--- reactNative_projeto1/src/config/ReactotronConfig.js
+++ reactNative_projeto1/src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.0.7' })
+  const tron = Reactotron.configure({ host: '192.168.0.4' })
     .useReactNative()
     .connect();
 
